d3c4d3d7cc3ce5eb186a84e5390dcc7a69f78266
Muhammad Saefudin <itsapud@gmail.com>
2022-12-14 13:37:41 +0700

    Initial commit

---
+++ src/logic/MainLogic.java
@@ -0,0 +1,11 @@
+package logic;
+
+import logic.logic01.*;
+import logic.logic02.*;
+
+public class MainLogic {
+    public static void main(String[] args) {
+        Logic02Soal09 soal = new Logic02Soal09(9);
+        soal.cetakArray();
+    }
+}
